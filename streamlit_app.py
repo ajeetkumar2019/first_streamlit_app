22eafba6bb8e734dbdc696b06511f497642ec63b
ajeetkumar2019 <59116235+ajeetkumar2019@users.noreply.github.com>
2023-02-26 18:32:36 +0530

    Update streamlit_app.py
    Used Pandas model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
+import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Parents New Healthy Diner')
+streamlit.dataframe(my_fruit_list)
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
