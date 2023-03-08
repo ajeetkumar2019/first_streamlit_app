79ba61188b8e1e2d2da6f7d49bb147b545fd608f
ajeetkumar2019 <59116235+ajeetkumar2019@users.noreply.github.com>
2023-03-08 19:20:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,13 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+##################################################################################
 #Function created for response
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-
+#############################################################################################
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -52,16 +52,16 @@ except URLError as e:
   
 ########################################
 streamlit.header("The fruit load list contains:")
-
+################################################################
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.excute("select * from pc_rivery_db.public.fruit_load_list")
+    my_cur.excute("select * from fruit_load_list")
     return my_cur.fetchall()
-  
+ ############################################################################
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 #my_cur = my_cnx.cursor()
